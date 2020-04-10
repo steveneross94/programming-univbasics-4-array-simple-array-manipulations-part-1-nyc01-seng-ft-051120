bcefa47898fba112ea929e0ccb50bff05daca570
Steven Ross <ross.steven@me.com>
2020-04-10 17:32:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,9 @@ def using_push(array, string)
   string = "violet"
   colors_in_the_rainbow.push(string)
 end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
